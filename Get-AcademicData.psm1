aad35548d99b6dbd177a1fbce771224a70a238c7
Will <64697894+Will-VS@users.noreply.github.com>
2020-12-25 18:40:15 +0800

    Update Get-AcademicData.psm1

--- Get-AcademicData.psm1
+++ Get-AcademicData.psm1
@@ -1,4 +1,38 @@
 <#
+.SYNOPSIS
+  Script will pull academic data for informationa dn fact checking - at a glance.
+.DESCRIPTION
+  Script will webs crape google scholar results using filters built into the powershell cmdlet
+  
+.PARAMETER Author
+
+.PARAMETER Tile
+
+.PARAMETER TopCited
+
+.PARAMETER Content
+
+.PARAMETER WebSite
+
+.PARAMETER BeforeDate
+
+.PARAMETER AfterDate
+
+.PARAMETER ExactContent
+
+.PARAMETER ExactTitle
 
 
+.INPUTS
+  <Inputs if any, otherwise state None>
+.OUTPUTS
+  <Outputs if any, otherwise state None - example: Log file stored in C:\Windows\Temp\<name>.log>
+.NOTES
+  Version:        1.0
+  Author:         <Name>
+  Creation Date:  <Date>
+  Purpose/Change: Initial script development
+  
+.EXAMPLE
+  <Example goes here. Repeat this attribute for more than one example>
 #>
